fc179d870621437fe49dd0d0e52d4eddb2890d84
wilson olabode <wilson olabode@WILSON>
2020-02-09 19:58:01 +0100

    added category enum

---
+++ TechBlog/Models/Enum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechBlog.Models
+{
+    public class Enum
+    {
+
+        public enum Category
+        {
+            ANDROID, TECHNOLOGY, PROGRAMMING, WEB, CAREER
+
+        }
+
+    }
+}
