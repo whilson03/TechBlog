a43dc2d3d885e0c66bf5a0655756c265330d36c8
wilson olabode <wilson olabode@WILSON>
2020-02-09 14:01:27 +0100

    added post table to db context

--- TechBlog/Data/ApplicationDbContext.cs
+++ TechBlog/Data/ApplicationDbContext.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TechBlog.Models;
 
 namespace TechBlog.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
+        public DbSet<Post> BlogPosts { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
