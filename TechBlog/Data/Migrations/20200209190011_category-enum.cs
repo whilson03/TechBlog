cc1a346dffb82e1cae278694652d0076b7e8a129
wilson olabode <wilson olabode@WILSON>
2020-02-09 20:03:55 +0100

    added categoty to model

---
+++ TechBlog/Data/Migrations/20200209190011_category-enum.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TechBlog.Data.Migrations
+{
+    public partial class categoryenum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Category",
+                table: "BlogPosts",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "BlogPosts");
+        }
+    }
+}
--- TechBlog/Models/Post.cs
+++ TechBlog/Models/Post.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using static TechBlog.Models.Enum;
 
 namespace TechBlog.Models
 {
@@ -29,5 +30,7 @@ namespace TechBlog.Models
         public int Likes { get; set; }
 
         public int Dislikes { get; set; }
+
+        public Category Category { get; set; }
     }
 }
