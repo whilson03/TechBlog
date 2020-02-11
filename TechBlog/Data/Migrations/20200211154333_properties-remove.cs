29032ea09d79186f5a53debc3bf6896ec4ab80e0
wilson olabode <wilson olabode@WILSON>
2020-02-11 16:47:47 +0100

    migration and update

---
+++ TechBlog/Data/Migrations/20200211154333_properties-remove.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TechBlog.Data.Migrations
+{
+    public partial class propertiesremove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dislikes",
+                table: "BlogPosts");
+
+            migrationBuilder.DropColumn(
+                name: "Likes",
+                table: "BlogPosts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Dislikes",
+                table: "BlogPosts",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Likes",
+                table: "BlogPosts",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
