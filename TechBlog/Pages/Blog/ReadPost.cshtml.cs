57e2b54652a02bf75880fe47d740dd4753d76b74
wilson olabode <wilson olabode@WILSON>
2020-02-11 16:01:31 +0100

    implemented reading post  and disqus

---
+++ TechBlog/Pages/Blog/ReadPost.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TechBlog.Models;
+
+namespace TechBlog
+{
+    public class ReadPostModel : PageModel
+    {
+        private readonly TechBlog.Data.ApplicationDbContext _context;
+
+        public ReadPostModel(TechBlog.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Post Post { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Post = await _context.BlogPosts.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Post == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
