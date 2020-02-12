a6a2e9a0799f550e922d24e6ac3d9a661c224a15
wilson olabode <wilson olabode@WILSON>
2020-02-12 15:32:47 +0100

    authorize admin operations

--- TechBlog/Pages/Admin/ManagePost/Edit.cshtml.cs
+++ TechBlog/Pages/Admin/ManagePost/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +12,7 @@ using TechBlog.Models;
 
 namespace TechBlog
 {
+    [Authorize]
     public class EditModel : PageModel
     {
         private readonly TechBlog.Data.ApplicationDbContext _context;
